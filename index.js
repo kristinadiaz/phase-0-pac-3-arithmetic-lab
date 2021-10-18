8c63320dec7df4eae5460c9371a3a96ed419cf23
Kristina Diaz <kristina.diaz84@gmail.com>
2021-10-18 14:17:42 -0600

    2nd attempt, completed assignment

--- index.js
+++ index.js
@@ -15,17 +15,19 @@ function divide(a, b) {
 }
 
 function increment(n) {
-    return n += 1;
+    n++;
+    return n;
 }
 
 function decrement(n) {
-    return n -= 1;
+    n--;
+    return n;
 }
 
-function makeInt(n) {
-    return parseInt(n, 10);
+function makeInt(string) {
+    return parseInt(string, 10);
 }
 
-function preserveDecimal(n) {
-    return parseFloat(n);
+function preserveDecimal(string) {
+    return parseFloat(string);
 }
